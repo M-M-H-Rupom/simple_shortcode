9f1c8cd521de95775256c1fe8a739f8c1a30758e
rupom <mohrajulrupom@gmail.com>
2024-03-14 15:25:09 +0600

    shortcode

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: simple Shortcode
+ * Author: Rupom
+ * Description: shortcode practise
+ * Version: 1.0
+ */
+function sht_button_details(){
+    echo "hello";
+}
+ add_action( "button_data","sht_button_details");
+?>
